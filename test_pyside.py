d9227caa18e507f57b76e250c007d9c91c3cee60
kngnepi <164445417+kngnepi@users.noreply.github.com>
2024-11-22 14:11:59 -0500

    updated

--- test_pyside.py
+++
@@ -1,8 +0,0 @@
-import sys
-sys.path.append(r'C:\Users\kendr\AppData\Local\Programs\FreeCAD 0.21\bin\Lib\site-packages')
-
-try:
-    from PySide2 import QtCore, QtWidgets
-    print("PySide2 imported successfully.")
-except ModuleNotFoundError as e:
-    print(f"Error: {e}")
